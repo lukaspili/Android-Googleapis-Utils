27b6a60c79f3d9a78915bc200ab8f967ee450375
Lukasz Piliszczuk <lukasz@siu-soon.com>
2012-07-06 20:39:54 +0200

    Fix on create.

--- src/com/siu/android/andgapisutils/activity/tracked/TrackedSherlockMapActivity.java
+++ src/com/siu/android/andgapisutils/activity/tracked/TrackedSherlockMapActivity.java
@@ -1,22 +1,21 @@
 package com.siu.android.andgapisutils.activity.tracked;
 
 import android.os.Bundle;
-import android.view.Window;
 import com.actionbarsherlock.app.SherlockMapActivity;
+import com.actionbarsherlock.view.Window;
 import com.google.android.apps.analytics.easytracking.EasyTracker;
-import com.siu.android.andgapisutils.map.EnhancedMapView;
 
 /**
  * @author Lukasz Piliszczuk <lukasz.pili AT gmail.com>
  */
 public class TrackedSherlockMapActivity extends SherlockMapActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState, int layout) {
         super.onCreate(savedInstanceState);
 
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-        setProgressBarIndeterminateVisibility(false);
+        setContentView(layout);
+        setSupportProgressBarIndeterminateVisibility(false);
 
         EasyTracker.getTracker().setContext(this);
     }
